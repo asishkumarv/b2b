c1b2fb759bc9dd7c61ee28dadaca161720b82964
asishkumarv <asishkumarv@outlook.com>
2026-06-12 18:15:23 +0530

    position of sidebar

--- Frontend/src/components/SocialSidebar.jsx
+++ Frontend/src/components/SocialSidebar.jsx
@@ -24,7 +24,7 @@ const SocialSidebar = () => {
       style={{
         position: 'fixed',
         left: 0,
-        top: '50%',
+        top: '80%',
         transform: 'translateY(-50%)',
         display: 'flex',
         flexDirection: 'column',
